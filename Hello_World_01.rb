a3dc766daf54653cfdf6a3540b9aaa5e371faa67
Minoru Kishi <HGA00562@nifty.com>
2016-10-28 22:51:46 +0900

    Update Hello_World_01.rb

--- Hello_World_01.rb
+++ Hello_World_01.rb
@@ -1,7 +1,20 @@
 #
 #file name:Hellow_World
 #
-
-  p "Hello World!"
+class Hello
+  attr_reader :name
+  def initialize(name = "World")
+    @name = name
+  end
   
+  def view
+    "Hello #{name}!"
+  end
+end
+
+h1 = Hello.new()
+puts h1.view
+
+h2 = Hello.new("Z80")
+puts h2.view
   
